9f78b3f2c6a742313e07d3097c66c3341b5ad850
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-03-05 19:51:45 +0300

    Added assets. Created navbar, activity list, dashboard and details components.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,9 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import axios from 'axios';
+import { NavBar } from '../../features/nav/NavBar';
+import { ActivityDashboard } from '../../features/activities/dashboard/ActivityDashboard';
 
 import { IActivity } from '../models/activity';
 
-import { Header, Icon, List } from 'semantic-ui-react';
+import { Header, Icon, List, Container } from 'semantic-ui-react';
 
 const App = () => {
   const [activities, setActivities] = useState<IActivity[]>([]);
@@ -16,17 +18,13 @@ const App = () => {
   }, []); // prevents endless loop
 
   return (
-    <div>
-      <Header as='h2'>
-        <Icon name='users' />
-        <Header.Content>Social Afalie</Header.Content>
-      </Header>
-      <List>
-        {activities.map(activity => (
-          <List.Item key={activity.id}>{activity.title}</List.Item>
-        ))}
-      </List>
-    </div>
+    // just like 'ng-container'
+    <Fragment>
+      <NavBar />
+      <Container style={{ marginTop: '7em' }}>
+        <ActivityDashboard activities={activities} />
+      </Container>
+    </Fragment>
   );
 }
 
---
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { ActivityList } from './ActivityList';
+
+import { IActivity } from '../../../app/models/activity';
+
+import { Grid } from 'semantic-ui-react';
+import { ActivityDetails } from '../details/ActivityDetails';
+
+interface IProps {
+  activities: IActivity[]
+}
+
+export const ActivityDashboard: React.FC<IProps> = ({activities}) => {
+  return (
+    <Grid>
+      <Grid.Column width={10}>
+        <ActivityList activities={activities} />
+      </Grid.Column>
+      <Grid.Column width={6}>
+        <ActivityDetails />
+      </Grid.Column>
+    </Grid>
+  )
+}
