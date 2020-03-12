ff95ef5d30e9c7453dccba03cda39ed26d9a8f61
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-03-12 12:23:34 +0300

    Created user activity table.

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -14,8 +14,23 @@ namespace Persistence
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder) ;
+
+            builder.Entity<UserActivity>(
+                x => x.HasKey(ua => new {ua.AppUserId, ua.ActivityId})
+            );
+
+            builder.Entity<UserActivity>()
+                .HasOne(u => u.AppUser)
+                .WithMany(a => a.UserActivities)
+                .HasForeignKey(u => u.AppUserId);
+
+            builder.Entity<UserActivity>()
+                .HasOne(u => u.Activity)
+                .WithMany(a => a.UserActivities)
+                .HasForeignKey(u => u.ActivityId);
         }
 
         public DbSet<Activity> Activities { get; set; }
+        public DbSet<UserActivity> UserActivities { get; set; }
     }
 }
