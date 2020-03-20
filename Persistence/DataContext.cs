43a4a4c760a5e48908d08e6f71bf58d50809fd89
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-03-20 15:29:40 +0300

    Created user following table. Setted relations.

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -27,11 +27,27 @@ namespace Persistence
                 .HasOne(u => u.Activity)
                 .WithMany(a => a.UserActivities)
                 .HasForeignKey(u => u.ActivityId);
+
+            builder.Entity<UserFollowing>(b =>
+            {
+                b.HasKey(u => new {u.ObserverId, u.TargetId});
+
+                b.HasOne(o => o.Observer)
+                    .WithMany(f => f.Followings)
+                    .HasForeignKey(o => o.ObserverId)
+                    .OnDelete(DeleteBehavior.Restrict);
+
+                b.HasOne(o => o.Target)
+                    .WithMany(f => f.Followers)
+                    .HasForeignKey(o => o.TargetId)
+                    .OnDelete(DeleteBehavior.Restrict);
+            });
         }
 
         public DbSet<Activity> Activities { get; set; }
         public DbSet<UserActivity> UserActivities { get; set; }
         public DbSet<Photo> Photos { get; set; }
         public DbSet<Comment> Comments { get; set; }
+        public DbSet<UserFollowing> Followings { get; set; }
     }
 }
